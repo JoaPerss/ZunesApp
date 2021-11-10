a4622a3ab87c54c9f93c5f03587caf2a07d55d66
JoaPerss <joachilp@hiof.no>
2021-11-10 15:51:59 +0100

    Added user fragment

---
+++ app/src/main/java/com/example/zunes/UserFragment.java
@@ -0,0 +1,40 @@
+package com.example.zunes;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class UserFragment extends Fragment {
+
+    public UserFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        toolbar();
+    }
+
+    private void toolbar() {
+        Toolbar toolbar;
+        toolbar = getView().findViewById(R.id.toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Zunes");
+    }
+}
