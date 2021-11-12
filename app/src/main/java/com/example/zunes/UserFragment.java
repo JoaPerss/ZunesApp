694f5e0f6ba6dbb87afc9a4f35759d1b8e933616
JoaPerss <joachilp@hiof.no>
2021-11-12 15:18:37 +0100

    Implemented User profile

--- app/src/main/java/com/example/zunes/UserFragment.java
+++ app/src/main/java/com/example/zunes/UserFragment.java
@@ -1,5 +1,9 @@
 package com.example.zunes;
 
+import static com.firebase.ui.auth.AuthUI.getApplicationContext;
+
+import android.annotation.SuppressLint;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,6 +15,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
+
+import java.util.Objects;
 
 public class UserFragment extends Fragment {
 
@@ -25,16 +40,52 @@ public class UserFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_user, container, false);
     }
 
+
+
+    @SuppressLint("RestrictedApi")
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        TextView displayName = view.findViewById(R.id.displayName);
+        TextView userEmail = view.findViewById(R.id.userEmail);
+        ImageView userImage = view.findViewById(R.id.userImage);
         super.onViewCreated(view, savedInstanceState);
         toolbar();
+
+        logOutUser();
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user!=null){
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            Uri photoUrl = user.getPhotoUrl();
+
+            displayName.setText(name);
+            userEmail.setText(email);
+            Picasso.get().load(photoUrl).into(userImage);
+
+        }else {
+            assert false;
+            Toast.makeText(getApplicationContext(), "Signed in as " +user.getDisplayName(), Toast.LENGTH_SHORT).show();
+        }
     }
+
+    private void logOutUser() {
+        Button button = requireView().findViewById(R.id.logOutButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("RestrictedApi")
+            @Override
+            public void onClick(View view) {
+                AuthUI.getInstance().signOut(getApplicationContext());
+                Toast.makeText(getContext(), "Signed out", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
 
     private void toolbar() {
         Toolbar toolbar;
-        toolbar = getView().findViewById(R.id.toolbar);
-        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
-        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Zunes");
+        toolbar = requireView().findViewById(R.id.toolbar);
+        ((AppCompatActivity) requireActivity()).setSupportActionBar(toolbar);
+        Objects.requireNonNull(((AppCompatActivity) requireActivity()).getSupportActionBar()).setTitle("Zunes");
     }
 }
